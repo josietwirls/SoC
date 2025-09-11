cf543a4d54dd06c4ecbb2f5a10935e3d225c609b
Josephine DAcquisto <128499878+josietwirls@users.noreply.github.com>
2025-09-11 09:55:02 -0500

    Add files via upload

---
+++ square_circuit_tb.sv
@@ -0,0 +1,48 @@
+`timescale 1ns/1ps
+
+module square_circuit_tb;
+    logic clk;
+    logic cw;
+    logic en;
+    logic rst_n;
+    logic [7:0] seg;    
+    logic [7:0] digit;  
+
+    // DUT instantiation
+    square_circuit dut (
+        .clk(clk),
+        .en(en),
+        .cw(cw),
+        .seg(seg),
+        .digit(digit),
+        .rst_n(rst_n)
+    );
+    
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+    
+    // stimulus
+    initial begin
+        cw = 00;
+        en = 0;
+        rst_n = 0;
+        
+        #20
+        en = 1;   // enable, clockwise
+
+        #20 
+        rst_n = 1;
+        
+        #20 
+        rst_n = 0;
+        
+        #200 
+        cw = 0;
+        
+        #200;
+        $finish;
+        
+    end
+endmodule
